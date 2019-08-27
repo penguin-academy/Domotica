731d0cc8462f1765001bf6fa276ed7c00def288e
Charlie P Dionisi <kranelox@gmail.com>
2019-08-27 04:34:42 -0400

    Added English translated files
    Translated to English
    - app.py
    - Readme file
    - index.html

--- app.py
+++ app.py
@@ -1,121 +1,121 @@
 #_*_ coding: utf-8 _*_
 """
 Autor: Williams Bobadilla
+Translation to English: Charlie Patino
 fecha de creacion: 2-abril-2019
 fecha de ultima edicion: 2-abril-2019   
 descripcion:proyecto basico de domotica para control de luces led con rasbperry pi
 notas: las lineas de codigo comentadas son para que al probar una maquina que no sea Raspberry Pi dee error, 
 a la hora de probar en la raspberry pi solo se debe de borrar los caracteres que comentan los bloques de codigo
-
-
 """
 
 
 
-import RPi.GPIO as gpio  	  #libreria para utilizar los puertos de entrada y salida
+import RPi.GPIO as gpio  	  #library to use the input/output ports
 from time import sleep
-from flask import Flask, render_template  #libreria del server a utilizar, Flask
+from flask import Flask, render_template  #Flask library, this is the server we will utilize
 
-app = Flask(__name__)  #Instanciamos el objeto Flask
+app = Flask(__name__)  #We make an instance of the Flask object
 
-msg=["Apagado", "Apagado ","Apagado","Apagado"]          #variable global para pasar mensajes del lado del cliente  
+msg=["Off", "Off ","Off","Off"]          #global variable for client-side messages
 
 
-led1=23       #definimos los pines de GPIO a utilizar
+led1=23       #we define the GPIO we will use
 led2=24
 led3=25
 led4=26
 
 
-gpio.setmode(gpio.BCM) 			# modo BCM de la raspberry pi
+gpio.setmode(gpio.BCM) 			#raspberry pi's BCM mode
  
 
 
     
-class Luces:                # esto es el docstring de la clase, seria como la documentacion
+class Lights:                #this is the class docstring, it's like the documentation
     """  
-        Clase para el manejo de luces
-        Posee 2 atributos: 
-           1. Ubicacion del led a controlar
-           2. puerto del gpio a utilizar
-        Posee 3 metodos:
-            1. encender()    
-            2. apagar()
-            3. intermitente(cantidad_intermitencia=2, tiempo=0.5), con este metodo debemos de tener cuidado ya que 
-         suspende la ejecucion del programa durante los sleeps(), deberiamos de hacerlo con hilos 
+        Class for the handling of lights
+         It has two atttributes:
+            1. Location of the LED we will control
+            2. GPIO port we will use
+         It has 3 methods:
+            1. on()
+            2. off()
+            3. blinking(blinking_freq=2, time=0.5), we must be careful with this method since it suspends the
+            execution of the program during the sleeps().
     """
     
-    def __init__(self,ubicacion,puerto):
-        self.ubicacion=ubicacion   # indicamos la ubicacion de la luz a controlar
-        self.puerto=puerto         #puerto del gpio a utilizar
-        gpio.setup(self.puerto,gpio.OUT)  #declaramos como salida al puerto declarado al instanciar el objeto
+    def __init__(self,location,port):
+        self.location=location   #we set the location of the light we want to control
+        self.port=port         #GPIO port we will use
+        gpio.setup(self.port,gpio.OUT)  #we set as the OUT PORT the port we set up during the instancing of the object
 
-    def encender(self):
-        gpio.output(self.puerto,True)   #encendemos el led  
+    def turnon(self):
+        gpio.output(self.port,True)   #we turn on the LED
     
-    def apagar(self):
-        gpio.output(self.puerto,False)  #apagamos el led 
+    def turnoff(self):
+        gpio.output(self.port,False)  #we turn off the LED
 
-    def intermitente(self, cantidad_intermitencia=2, tiempo=0.5):  
-        for a in range(cantidad_intermitencia):
-            gpio.output(self.puerto,True)  #encendemos
-            sleep(tiempo)                  #esperamos un tiempo
-            gpio.output(self.puerto,False)
-            sleep(tiempo)
+    def blinking(self, blinking_freq=2, time=0.5):  
+        for a in range(blinking_freq):
+            gpio.output(self.port,True)  #we turn it on
+            sleep(time)                  #we wait a while
+            gpio.output(self.port,False)
+            sleep(time)
 
 
-luces1=Luces("Dormitorio",led1) #instanciamos el objeto Luces y eenviamos los parametros ubicacion y el puerto a utilizar del gpio
-luces2=Luces("Sala",led2)
-luces3=Luces("Cocina",led3)
-luces4=Luces("Patio",led4)
+lights1=Lights("Bedroom",led1) #we make an instance of the object Lights and send the paramenters location and the GPIO port we will use
+lights2=Lights("LivingRoom",led2) 
+lights3=Lights("Kitchen",led3)
+lights4=Lights("Patio",led4)
 
 
 
 @app.route('/<int:estado>')
-def index1(estado):         #recibimos el estado enviado por el cliente
+def index1(status):         #we get the status sent by the client
     
-    if estado==0:
-        msg[0]="Encendido"  #encendemos si recibimos un cero de parte del cliente
-        luces1.encender()   #utilizamos el metodo encender del objeto Luces
-    elif estado==1:
-        msg[0]="Apagado"   #apagamos si recibimos un 1 de parte del cliente 
-        luces1.apagar()
-    elif estado==2:
-        msg[1]="Encendido"
-        luces2.encender()
-    elif estado==3:
-        msg[1]="Apagado"
-        luces2.apagar()
-    elif estado==4:
-        msg[2]="Encendido"
-        luces3.encender()
-    elif estado==5:
-        msg[2]="Apagado"
-        luces3.apagar()
-    elif estado==6:
-        msg[3]="Encendido"
-        luces4.encender()
-    elif estado==7:
-        msg[3]="Apagado"
-        luces4.apagar()
+    if status==0:
+        msg[0]="On"  #we turn it on if we receive a 0 from the client
+        lights1.turnon()   #we utilize the turnon method from the object Lights
+    elif status==1:
+        msg[0]="Off"   #we turn it off if the client sends a 1
+        lights1.turnoff()
+    elif status==2:
+        msg[1]="On"
+        lights2.turnoff()
+    elif status==3:
+        msg[1]="Off"
+        lights2.turnoff()
+    elif status==4:
+        msg[2]="On"
+        lights3.turnon()
+    elif status==5:
+        msg[2]="Off"
+        lights3.turnoff()
+    elif status==6:
+        msg[3]="On"
+        lights4.turnon()
+    elif status==7:
+        msg[3]="Off"
+        lights4.turnoff()
 
     dato={
-         "dormitorio":msg[0],
-         "sala":msg[1],
-         "cocina":msg[2],
+         "bedroom":msg[0],
+         "livingroom":msg[1],
+         "kitchen":msg[2],
          "patio":msg[3]
 
-    }                  #utilizado para enviar los estados de los controles 
-    return render_template('index.html',msg=dato)   #renderizamos la pagina y enviamos info del lado del cliente
+    }                  #we use it to send the status of the controls
+    return render_template('index.html',msg=dato)   #we render the webpage and send the info to the client
+
 
 
 @app.route('/')
-def inicio():
+def start():
     
     dato={
-         "dormitorio":msg[0],
-         "sala":msg[1],
-         "cocina":msg[2],
+         "bedroom":msg[0],
+         "livingroom":msg[1],
+         "kitchen":msg[2],
          "patio":msg[3]
 
     }
@@ -125,4 +125,3 @@ def inicio():
 
 if __name__ == '__main__':
    app.run(debug = True)
-
